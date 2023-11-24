4b61ed81807321ccc397dcb21ada5d7624c1dc1d
Han <xuhan0325@gmail.com>
2023-11-24 23:46:28 +0800

    Spinner initial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,16 @@ plugins {
 
 android {
     namespace = "com.example.yingaccouting"
-    compileSdk = 33
+    compileSdk = 34
+
+    viewBinding{
+        enable = true
+    }
 
     defaultConfig {
         applicationId = "com.example.yingaccouting"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/yingaccouting/MainActivity.kt
+++ app/src/main/java/com/example/yingaccouting/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.yingaccouting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.yingaccouting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Spinner值設定
+        val spinner_adapter = ArrayAdapter.createFromResource(this,R.array.type,
+            com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
+        binding.TypeSpinner.adapter = spinner_adapter
+
+
     }
 }
